efce9e934fa63d03c7970619c6686c00ba60806d
Nicholas Smith <npsmith1990@gmail.com>
2016-01-27 22:59:20 -0600

    Initial Commit

---
+++ Valentine/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Valentine
+//
+//  Created by Nick on 1/27/16.
+//  Copyright © 2016 Nick. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
