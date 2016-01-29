6e959d23edac8c6ec9bf51699c1d706cc8dc143a
Nicholas Smith <npsmith1990@gmail.com>
2016-01-29 14:38:49 -0600

    Restructure how songs are saved. Fix array out of bounds exception with prev button; implement tableView boilerplate

--- Valentine/FirstViewController.swift
+++ Valentine/FirstViewController.swift
@@ -9,20 +9,52 @@
 import UIKit
 import AVFoundation
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
+    @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var currentSongLbl: UILabel!
+    @IBOutlet weak var currentArtistLbl: UILabel!
     
     var songCounter = 0
     var musicPlayer: AVAudioPlayer!
-    var songs = ["cruise","youandi","anywherewithyou","yourman"]
-    var songNames = ["Cruise", "You and I", "Anywhere With You", "Your Man"]
+//    var songs = ["cruise","youandi","anywherewithyou","yourman"]
+//    var songNames = ["Cruise", "You and I", "Anywhere With You", "Your Man"]
+    
+    var songs = [
+        ["name":"Cruise","artist":"Florida Georgia Line","filename":"cruise"],
+        ["name":"You and I","artist":"Ingrid Michaelson","filename":"youandi"],
+        ["name":"Anywhere With You","artist":"Josh Owen","filename":"anywherewithyou"],
+        ["name":"Your Man","artist":"Josh Turner","filename":"yourman"]
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        playSong(songs[songCounter])
+        playSong(songs[songCounter]["filename"]!)
     }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        // @TODO
+        return UITableViewCell()
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        // @TODO
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return songs.count
+    }
+    
+    
+    
+    //              //
+    // Music Player //
+    //              //
     
     func playSong(song: String) {
         
@@ -33,30 +65,29 @@ class FirstViewController: UIViewController {
             musicPlayer.prepareToPlay()
             musicPlayer.numberOfLoops = 1
             musicPlayer.play()
-            currentSongLbl.text = songNames[songCounter]
+            currentSongLbl.text = songs[songCounter]["name"]!
         } catch let err as NSError {
             currentSongLbl.text = "Oops... something went wrong"
             print(err.debugDescription)
         }
-    }
-    
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     @IBAction func nextPressed(sender: UIButton) {
         songCounter++
         songCounter %= songs.count
         musicPlayer.stop()
-        playSong(songs[songCounter])
+        playSong(songs[songCounter]["filename"]!)
     }
+    
     @IBAction func prevPressed(sender: AnyObject) {
         songCounter--
         songCounter %= songs.count
+        if songCounter < 0 {
+            songCounter = songs.count - 1
+        }
         musicPlayer.stop()
-        playSong(songs[songCounter])
+        print(songCounter)
+        playSong(songs[songCounter]["filename"]!)
     }
 
     @IBAction func playPausePressed(sender: AnyObject) {
