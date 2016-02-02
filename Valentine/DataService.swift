e939c17457116d7ab6c45f85b7f3210d92434a4e
Nicholas Smith <npsmith1990@gmail.com>
2016-02-01 20:11:47 -0600

    Create save and load funcs for saving Gifts using userdefaults

--- Valentine/DataService.swift
+++ Valentine/DataService.swift
@@ -17,9 +17,33 @@ class DataService {
     private var _loadedGifts = [Gift]()
     
     var loadedPosts: [Gift] {
-        return _loadedPosts
+        return _loadedGifts
     }
     
     private var _giftNumber: Int = 0
+    
+    var giftNumber: Int {
+        get {
+            return _giftNumber
+        }
+        set {
+            _giftNumber = newValue
+        }
+    }
+    
+    func savePosts() {
+        let giftsData = NSKeyedArchiver.archivedDataWithRootObject(_loadedGifts)
+        NSUserDefaults.standardUserDefaults().setObject(giftsData, forKey: GIFTS_KEY)
+        NSUserDefaults.standardUserDefaults().synchronize()
+    }
+    
+    func loadPosts() {
+        if let giftsData = NSUserDefaults.standardUserDefaults().objectForKey(GIFTS_KEY) as? NSData {
+            if let giftsArray = NSKeyedUnarchiver.unarchiveObjectWithData(giftsData) as? [Gift] {
+                _loadedGifts = giftsArray
+            }
+        }
+        NSNotificationCenter.defaultCenter().postNotification(NSNotification(name: "giftsLoaded", object: nil))
+    }
 
 }
