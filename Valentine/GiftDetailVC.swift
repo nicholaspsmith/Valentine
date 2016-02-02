ae1fe88f25e4e1402d37b2f757405ccc3c412016
Nicholas Smith <npsmith1990@gmail.com>
2016-02-01 19:03:33 -0600

    Add back button to gift detail view

--- Valentine/GiftDetailVC.swift
+++ Valentine/GiftDetailVC.swift
@@ -22,5 +22,7 @@ class GiftDetailVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    @IBAction func cancelPressed(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 }
