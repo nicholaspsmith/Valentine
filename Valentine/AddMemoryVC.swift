0e2a865acc6a53efe0d4d670e9661ef66981b399
Nicholas Smith <npsmith1990@gmail.com>
2016-02-11 22:58:51 -0600

    Add placeholder text to textview on memory add controller

--- Valentine/AddMemoryVC.swift
+++ Valentine/AddMemoryVC.swift
@@ -8,14 +8,19 @@
 
 import UIKit
 
-class AddMemoryVC: UIViewController {
+class AddMemoryVC: UIViewController, UITextViewDelegate {
 
     @IBOutlet weak var textView: UITextView!
+    
+    let PLACEHOLDER_TEXT = "What is your memory?"
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        textView.delegate = self
+        
+        applyPlaceholderStyle(self.textView!, placeholderText: PLACEHOLDER_TEXT)
     }
 
     override func didReceiveMemoryWarning() {
@@ -37,6 +42,67 @@ class AddMemoryVC: UIViewController {
                 textView.text = ""
                 dismissViewControllerAnimated(true, completion: nil)
             }
+        }
+    }
+    
+    func textViewShouldBeginEditing(aTextView: UITextView) -> Bool
+    {
+        if aTextView == self.textView && aTextView.text == PLACEHOLDER_TEXT
+        {
+            // move cursor to start
+            moveCursorToStart(aTextView)
+        }
+        return true
+    }
+    
+    func moveCursorToStart(aTextView: UITextView)
+    {
+        dispatch_async(dispatch_get_main_queue(), {
+            aTextView.selectedRange = NSMakeRange(0, 0);
+        })
+    }
+    
+    func applyPlaceholderStyle(aTextview: UITextView, placeholderText: String)
+    {
+        // make it look (initially) like a placeholder
+        aTextview.textColor = UIColor.lightGrayColor()
+        aTextview.text = placeholderText
+    }
+    
+    func applyNonPlaceholderStyle(aTextview: UITextView)
+    {
+        // make it look like normal text instead of a placeholder
+        aTextview.textColor = UIColor.darkTextColor()
+        aTextview.alpha = 1.0
+    }
+    
+    func textView(textView: UITextView, shouldChangeTextInRange range: NSRange, replacementText text: String) -> Bool {
+        // remove the placeholder text when they start typing
+        // first, see if the field is empty
+        // if it's not empty, then the text should be black and not italic
+        // BUT, we also need to remove the placeholder text if that's the only text
+        // if it is empty, then the text should be the placeholder
+        let newLength = textView.text.utf16.count + text.utf16.count - range.length
+        if newLength > 0 // have text, so don't show the placeholder
+        {
+            // check if the only text is the placeholder and remove it if needed
+            // unless they've hit the delete button with the placeholder displayed
+            if textView == self.textView && textView.text == PLACEHOLDER_TEXT
+            {
+                if text.utf16.count == 0 // they hit the back button
+                {
+                    return false // ignore it
+                }
+                applyNonPlaceholderStyle(textView)
+                textView.text = ""
+            }
+            return true
+        }
+        else  // no text, so show the placeholder
+        {
+            applyPlaceholderStyle(textView, placeholderText: PLACEHOLDER_TEXT)
+            moveCursorToStart(textView)
+            return false
         }
     }
 
